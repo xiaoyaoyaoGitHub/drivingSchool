b9243b45d9d5e55671d1731a0f3ff8cffc092d12
wangluyao <wangluyao959277@163.com>
2023-05-11 17:24:53 +0800

    feat:预约课程增加会员身份判断

--- src/pages/appointment/appointment.js
+++ src/pages/appointment/appointment.js
@@ -2,7 +2,7 @@
  * @Author: wangluyao wangluyao959277@163.com
  * @Date: 2023-03-08 22:10:04
  * @LastEditors: wangluyao wangluyao959277@163.com
- * @LastEditTime: 2023-05-06 09:29:00
+ * @LastEditTime: 2023-05-11 17:18:54
  * @FilePath: /wxapp-boilerplate/src/pages/appointment/appointment.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -123,5 +123,13 @@ Page({
 		} catch (err) {
 			console.log(`err`, err);
 		}
+	}),
+	/**
+	 * 支付VIP
+	 */
+	payVip: throttle(function(){
+		wx.navigateTo({
+			url: '/pages/payInfo/payInfo',
+		});
 	}),
 });
